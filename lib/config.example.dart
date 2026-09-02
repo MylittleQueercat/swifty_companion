28b61ec01c5b57c4572ce8915390cd108c566ca0
hguo <hguo@f1br7s7.paris.42.school>
2026-09-02 16:12:46 +0200

    Add config template

---
+++ lib/config.example.dart
@@ -0,0 +1,5 @@
+class Config {
+  static const String clientId = "YOUR_42_API_UID";
+  static const String clientSecret = "YOUR_42_API_SECRET";
+  static const String redirectUri = "swiftycompanion://callback";
+}
